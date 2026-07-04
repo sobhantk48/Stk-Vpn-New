f7b2d2323f6a1f46f2cb5ab01ace1cb9e8d835fa
sobhantk48 <sobhantk48@gmail.com>
2026-07-04 14:29:53 +0330

    fix: align kotlin and serialization plugin versions to 1.9.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,11 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // flatDir دیگر نیاز نیست
     }
 }
-rootProject.name = "stkvpn"
+rootProject.name = "Stkvpn"
 include(":app")

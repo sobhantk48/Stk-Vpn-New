b6c41b732dde56405a5521729da58cdab5b7e2ac
sobhantk48 <sobhantk48@gmail.com>
2026-07-07 21:39:10 +0330

    final: integrate libbox classes and native lib for Sing-box core

--- app/src/main/java/com/v2ray/app/v2ray/SingBoxManager.kt
+++ app/src/main/java/com/v2ray/app/v2ray/SingBoxManager.kt
@@ -148,7 +148,6 @@ class SingBoxManager(private val context: Context) {
                     stopV2Ray().getOrThrow()
                 }
 
-                // استفاده از Libbox.newBoxInstance با LocalResolver
                 val box = Libbox.newBoxInstance(configJson, LocalResolver)
                 boxInstance = box
                 box.start()
